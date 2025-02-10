c3adc59bbb02cb1999303a8e8f7acc8d72cb1419
LORENZO DIROSA s329169 <s329169@studenti.polito.it>
2025-02-10 10:42:27 +0100

    commit

---
+++ s329169.py
@@ -0,0 +1,32 @@
+from numpy import cos, exp, log, sin, tan, sqrt
+
+def f1(x):  # 0 with population_size=10k
+    return sin(x[0])
+
+
+def f2(x):  # 18,6e12 with population_size=20k
+    return (((x[0] / ((((sin((-8 - x[1])) - (-9 / -9)) + 9) * exp(-1)) + (cos(x[2]) / tan((-3 ^ 9))))) * exp(8)) * exp((log(8) * tan(-5))))
+
+
+def f3(x):  # 4,6  with population_size=20k
+    return ((((x[0] ^ 2) * log(log(exp(7)))) - exp(-2)) + (((6 - x[2]) - (2 * x[2])) - (x[1] ^ 3)))
+
+
+def f4(x): # 0,07 with population_size=10k
+    return ((cos(x[1]) * (7 ^ 1)) + exp(sqrt(sqrt(2))))
+
+
+def f5(x): # 2,18e-19 with population_size=10k 
+    return (((3 ^ 6) ^ (-8 / x[1])) / ((x[0] + -9) ^ 7))
+
+
+def f6(x):  #  0,00114 with population_size=10k 
+    return ((sqrt(sqrt(8)) * x[1]) - ((5 + sqrt(log((2 + 2)))) * (x[0] / exp(tan(-2)))))
+
+
+def f7(x):  # 330,14 wirh population_size=15k
+    return exp(((x[0] * x[1]) - (-6 / 4)))
+
+
+def f8(x):  # 6.4e5 with population_size=20k
+    return ((x[5] ^ 4) * ((-6 / 4) + (x[5] * 5)))
